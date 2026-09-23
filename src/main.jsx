67f62c42d1d0971422cda6915b72e2588fd85dd6
yousufkhan46u <yousufkhan46u@gmail.com>
2026-09-23 05:21:53 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -772,7 +772,7 @@ function Profile({user,profile,setProfile}){
 
    const {error:uploadError}=await supabase
     .storage
-    .from("avatars")
+    .from("avatar")
     .upload(path,file,{
      upsert:true,
      contentType:file.type
